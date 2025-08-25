80b90bdec1deccaf3b7638fc13190843e17a35f4
Balabharathi2005 <168760073+Balabharathi2005@users.noreply.github.com>
2025-08-25 23:36:13 +0530

    Time: 2 ms (93.97%), Space: 46.8 MB (50.86%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,23 +1,8 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] ans = new int[n+1];
-        if(n==0) {
-            ans[0]=0;
-            return ans;
-        }
-        long ints;
-        int count=0;
-        String binary;
-        for(int i=1;i<=n;i++){
-            count=0;
-           binary=Integer.toBinaryString(i);
-           ints=Long.parseLong(binary);
-           while(ints!=0){
-            long temp=ints%10;
-            count+=temp;
-            ints/=10;
-           }
-           ans[i]=count;
+        int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
         }
         return ans;
     }
